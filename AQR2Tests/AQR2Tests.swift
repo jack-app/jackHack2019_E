040360a3aff29d3abaf736ee7292347cf569c54d
nakata yuki <nasutaro211@gmail.com>
2019-05-04 17:31:17 +0900

    first commit

---
+++ AQR2Tests/AQR2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AQR2Tests.swift
+//  AQR2Tests
+//
+//  Created by 中田　優樹 on 2019/05/04.
+//  Copyright © 2019年 nakatayuki. All rights reserved.
+//
+
+import XCTest
+@testable import AQR2
+
+class AQR2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
